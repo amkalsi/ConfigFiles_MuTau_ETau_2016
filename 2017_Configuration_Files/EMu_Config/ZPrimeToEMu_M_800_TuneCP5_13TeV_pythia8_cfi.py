41b65c86040245f37135323d8b5c5c8843ab30a9
Amandeep Kaur Kalsi <rimmy330@gmail.com>
2018-05-12 16:30:01 +0200

    Added 2017 config files

---
+++ 2017_Configuration_Files/EMu_Config/ZPrimeToEMu_M_800_TuneCP5_13TeV_pythia8_cfi.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+        comEnergy = cms.double(13000.0),
+        crossSection = cms.untracked.double(1),
+        filterEfficiency = cms.untracked.double(1),
+        maxEventsToPrint = cms.untracked.int32(0),
+        pythiaHepMCVerbosity = cms.untracked.bool(False),
+        pythiaPylistVerbosity = cms.untracked.int32(1),
+        PythiaParameters = cms.PSet(
+                pythia8CommonSettingsBlock,
+                pythia8CP5SettingsBlock,
+                processParameters = cms.vstring(
+                        'NewGaugeBoson:ffbar2gmZZprime= on',         #get new gauge boson process 
+                        'Zprime:gmZmode = 3',                        #only Zprime contributions without any interferences
+                        '32:m0= 800',                               #set pole mass of resonancei
+                        '32:addChannel= 1 0.1 100 11 -13',
+                        '32:addChannel= 1 0.1 100 13 -11',
+                        '32:onMode= off',                            #set default to switched-off decay modes
+                        '32:onIfMatch= 13 -11',                      #allow decay to Tau + electron 
+                        '32:onIfMatch= 11 -13',
+                ),
+                parameterSets = cms.vstring('pythia8CommonSettings',
+                                            'pythia8CP5Settings',
+                                            'processParameters')
+        )
+)
+ProductionFilterSequence = cms.Sequence(generator)
---
+++ 2017_Configuration_Files/ETau_Config/ZPrimeToETau_M_2400_TuneCP5_13TeV_pythia8_cfi.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+# for taus
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import * 
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+        comEnergy = cms.double(13000.0),
+        crossSection = cms.untracked.double(1),
+        filterEfficiency = cms.untracked.double(1),
+        maxEventsToPrint = cms.untracked.int32(0),
+        pythiaHepMCVerbosity = cms.untracked.bool(False),
+        pythiaPylistVerbosity = cms.untracked.int32(1),
+        ExternalDecays = cms.PSet(Tauola = cms.untracked.PSet(TauolaPolar, TauolaDefaultInputCards ), 
+                                                              parameterSets = cms.vstring('Tauola') 
+                                                             ), 
+        PythiaParameters = cms.PSet(
+                pythia8CommonSettingsBlock,
+                pythia8CP5SettingsBlock,
+                processParameters = cms.vstring(
+                        'NewGaugeBoson:ffbar2gmZZprime= on',         #get new gauge boson process 
+                        'Zprime:gmZmode = 3',                        #only Zprime contributions without any interferences
+                        '32:m0= 2400',                               #set pole mass of resonancei
+                        '32:addChannel= 1 0.1 100 11 -15',
+                        '32:addChannel= 1 0.1 100 15 -11',
+                        '32:onMode= off',                            #set default to switched-off decay modes
+                        '32:onIfMatch= 15 -11',                      #allow decay to Tau + electron 
+                        '32:onIfMatch= 11 -15',
+                ),
+                parameterSets = cms.vstring('pythia8CommonSettings',
+                                            'pythia8CP5Settings',
+                                            'processParameters')
+        )
+)
+ProductionFilterSequence = cms.Sequence(generator)
